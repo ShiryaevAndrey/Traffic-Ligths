bbef245992ad4d98878436778f6a40b78d161e81
SevenSkynet <xandreix@yandex.ru>
2022-03-02 23:40:41 +0500

    Initial Commit

---
+++ Traffic Ligths/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Ligths
+//
+//  Created by Andrey Shiryaev on 02.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
